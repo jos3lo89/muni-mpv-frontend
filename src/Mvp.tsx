b71619c9fdc4f7e74db16c0180e8cf6287600332
jos3lo89 <ganbaru743@gmail.com>
2025-11-14 16:21:46 -0500

    update: shadcn dark theme

--- src/Mvp.tsx
+++ src/Mvp.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/routes";
-import { ThemeProvider } from "./components/theme-provider";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const Mvp = () => {
   return (
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { ModeToggle } from "@/components/mode-toggle";
+import { ModeToggle } from "@/components/ModeToggle";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
